6da4e12302c0ee41fddd07a0bed06b169f34fc8e
Zykiria01 <93816709+Zykiria01@users.noreply.github.com>
2023-01-03 22:07:25 +0700

    Add files via upload

---
+++ Warungku/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login | Warungku</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <div class="box-login">
+        <h2>Login</h2>
+        <form>
+            <input type="text" name="user" placeholder="Username">
+            <input type="password" name="pass" placeholder="Password">
+            <input type="submit" name="submit" value="Login">
+        </form>
+    </div>
+</body>
+</html>
